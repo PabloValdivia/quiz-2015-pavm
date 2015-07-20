19a8205f0f3c72435d3c88f18cf38824acbe7e5a
Pablo Valdivia <pablo.valdivia@egs.pe>
2015-07-20 18:11:25 -0500

    Sass y Bootstrap (header && Footer)

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET autor page. */
+router.get('/autor', function(req, res) {
+ res.render('autor', { title: 'Autores' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
